6e6b58fdbb66da7d41a69d8bf07bce7246ec6f5a
MikiP98 <pokora.mikolaj@gmail.com>
2024-01-03 19:55:18 +0100

    Loaded the template

---
+++ src/client/java/io/github/mikip98/ShimmerSupportLayerClient.java
@@ -0,0 +1,10 @@
+package io.github.mikip98;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShimmerSupportLayerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
